f97639b8d708fdadf18160ea9c1b718fd02d916a
ashima0411 <101987927+ashima0411@users.noreply.github.com>
2022-06-23 12:31:47 +0530

    Update exp19.php

--- exp19.php
+++ exp19.php
@@ -1,5 +1,5 @@
 <?php
-$myfile = fopen("dhruv.txt", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("dhruv.txt"));
+$myfile = fopen("file.txt", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("file.txt"));
 fclose($myfile);
 ?>
