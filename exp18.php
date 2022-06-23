0ce412f7ffdf0f05d715f576dc49483eba7c54a0
ashima0411 <101987927+ashima0411@users.noreply.github.com>
2022-06-23 12:31:02 +0530

    Update exp18.php

--- exp18.php
+++ exp18.php
@@ -1,10 +1,10 @@
 <?php
-print_r("Dhruv");   //lib fun
+print_r("ashima");   //lib fun
 echo"<br>";
-echo readfile("dhruv.txt");   //lib fun
+echo readfile("file.txt");   //lib fun
 $a=1;
 echo"<br>";
 var_dump($a);   //lib fun
 echo"<br>";
-echo strlen('Hello I am Dhruv. Welcome to my program');    //lib fun
+echo strlen('Hello I am ashima. Welcome to my program');    //lib fun
 ?>
